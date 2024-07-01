b14e021a346c2ac6c7f7855682d0c11b71d76a73
Guilherme Marmiroli <guilherme.marmiroli@hotmail.com>
2024-06-30 21:04:48 -0300

    Notes and Easels

---
+++ src/components/easels/svelte.config.js
@@ -0,0 +1,18 @@
+/**
+ * from https://kit.svelte.dev/docs/adapter-static
+ */
+import adapter from '@sveltejs/adapter-static';
+
+export default {
+    kit: {
+        adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically — see below
+            pages: 'build',
+            assets: 'build',
+            fallback: null,
+            precompress: false,
+            strict: true
+        })
+    }
+};
