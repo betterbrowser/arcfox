6e71a9778f63076d86d504ffe2a29e54c42b4272
Guilherme Marmiroli <guilherme.marmiroli@hotmail.com>
2024-08-12 16:19:55 -0300

    Peeks can now be closed

--- src/peek/peek.js
+++ src/peek/peek.js
@@ -1,11 +1,32 @@
 var peekPage = document.createElement("div");
 peekPage.id = "peekpage";
 peekPage.popover = true;
-peekPage.style = "height: 100vh; width: 70vw; margin: auto; border: 0; padding: 0; animation: peekopen 0.5s; "
+
+var peekBackdrop = document.createElement('div');
+peekBackdrop.style = "display: none; height: 100vh; width: 100vw; top: 0; left: 0; position: fixed; z-index: 2147483647;";
+peekBackdrop.onclick = () => closePeek();
+
+var tools = [{ name: 'close' }]
+tools.forEach((tool) => {
+    var btn = document.createElement('button');
+    btn.innerHTML = tool.name
+    btn.className = 'peektools'
+    if (tool.name == 'close') {
+        btn.onclick = () => closePeek()
+    }
+    peekPage.appendChild(btn);
+})
+
+function closePeek() {
+    peekPage.hidePopover();
+    document.body.style.overflow = ''
+    peekBackdrop.style.display = 'none'
+}
+
+document.body.appendChild(peekBackdrop);
 document.body.appendChild(peekPage);
 
 var peekIframe = document.createElement("iframe");
-peekIframe.frameBorder = 0;
 peekPage.appendChild(peekIframe);
 
 let collection = document.getElementsByTagName("a");
@@ -17,8 +38,16 @@ Array.from(collection).forEach(function (element) {
         if (event.shiftKey) {
             event.preventDefault();
             element.href = "javascript:;";
+            element.jsaction = '';
+            element.target = ''
             peekIframe.src = oldhref;
+            peekBackdrop.style.display = 'block';
+            document.body.style.overflow = 'hidden'
             peekPage.showPopover();
+        } else {
+            if (element.href == "javascript:;") {
+                element.href = oldhref
+            }
         }
     };
 });
