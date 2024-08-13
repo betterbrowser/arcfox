01f24c213456c726d0a32339598dd3f5e17c954d
Guilherme Marmiroli <guilherme.marmiroli@hotmail.com>
2024-08-12 21:22:46 -0300

    You can now open Peek in fullscreen!

--- src/peek/peek.js
+++ src/peek/peek.js
@@ -6,21 +6,30 @@ var peekBackdrop = document.createElement('div');
 peekBackdrop.style = "display: none; height: 100vh; width: 100vw; top: 0; left: 0; position: fixed; z-index: 2147483647;";
 peekBackdrop.onclick = () => closePeek();
 
-var tools = [{ name: 'close' }]
+var tools = [{ name: 'close' }, { name: 'open_in_full' }]
 tools.forEach((tool) => {
     var btn = document.createElement('button');
     btn.innerHTML = tool.name
     btn.className = 'peektools'
+    btn.id = tool.name
     if (tool.name == 'close') {
         btn.onclick = () => closePeek()
+    } else {
+        btn.onclick = () => {
+            closePeek();
+            window.open(iframeOldSrc);
+        }
     }
     peekPage.appendChild(btn);
 })
 
+var iframeOldSrc = ""
 function closePeek() {
     peekPage.hidePopover();
     document.body.style.overflow = ''
     peekBackdrop.style.display = 'none'
+    iframeOldSrc = peekIframe.src
+    peekIframe.src = ""
 }
 
 document.body.appendChild(peekBackdrop);
