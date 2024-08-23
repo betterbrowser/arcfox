bb2134ee2dcd83310bdf768575debf3b78324e71
Guilherme Marmiroli <guilherme.marmiroli@hotmail.com>
2024-08-23 19:49:49 -0300

    Customizable favorites

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -1,26 +1,41 @@
 browser.storage.local.get('favorites', function (result) {
   var favorites = result.favorites || [{ url: 'https://gmail.com', favicon: 'https://mailmeteor.com/logos/assets/PNG/Gmail_Logo_512px.png', id: 0 }, { url: 'https://music.youtube.com', favicon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Youtube_Music_icon.svg/2048px-Youtube_Music_icon.svg.png', id: 1 }];
-  document.querySelector('#f1').value = favorites[0].url;
-  document.querySelector('#f2').value = favorites[1].url;
-  console.log(result.favorites)
+  if (favorites[0] !== undefined) {
+    document.querySelector('#f1').value = favorites[0].url;
+  }
+  if (favorites[1] !== undefined) {
+    document.querySelector('#f2').value = favorites[1].url;
+  }
 });
 
 document.querySelector('#btn').addEventListener('click', () => {
-  browser.windows.getCurrent({ populate: true }).then((window) => {
-    browser.windows.remove(window.id);
-  });
   browser.storage.local.get('favorites', function (result) {
     var favoritesc = result.favorites || [{ url: 'https://gmail.com', favicon: 'https://mailmeteor.com/logos/assets/PNG/Gmail_Logo_512px.png', id: 0 }, { url: 'https://music.youtube.com', favicon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Youtube_Music_icon.svg/2048px-Youtube_Music_icon.svg.png', id: 1 }];
-    if (favoritesc[0].url !== document.querySelector('#f1').value) {
-      favoritesc[0].url = document.querySelector('#f1').value
-      favoritesc[0].favicon = 'undefined';
-    }
-    if (favoritesc[1].url !== document.querySelector('#f2').value) {
-      favoritesc[1].url = document.querySelector('#f2').value
-      favoritesc[1].favicon = 'undefined';
-    }
+
+    [0, 1].forEach((i) => {
+      if (favoritesc[i] == undefined) {
+        if (document.querySelector('#f' + (i + 1)).value !== "") {
+          favoritesc[i] = {}
+          favoritesc[i].url = document.querySelector('#f' + (i + 1)).value
+          favoritesc[i].favicon = 'https://i0.wp.com/www.flyycredit.com/wp-content/uploads/2018/06/globe-icon-white.png?fit=512%2C512&ssl=1';
+          favoritesc[i].id = i;
+        }
+      } else {
+        if (document.querySelector('#f' + (i + 1)).value !== "" && document.querySelector('#f' + (i + 1)).value !== favoritesc[i].url) {
+          favoritesc[i].url = document.querySelector('#f' + (i + 1)).value
+          favoritesc[i].favicon = 'https://i0.wp.com/www.flyycredit.com/wp-content/uploads/2018/06/globe-icon-white.png?fit=512%2C512&ssl=1';
+        }
+      }
+
+      if (document.querySelector('#f' + (i + 1)).value == "") {
+        delete favoritesc[i]
+      }
+    })
     browser.storage.local.set({
       favorites: favoritesc
     });
   })
+  browser.windows.getCurrent({ populate: true }).then((window) => {
+    browser.windows.remove(window.id);
+  });
 })
--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -20,6 +20,7 @@ newTabButton.addEventListener("click", () =>
 // Get favorites from localstorage
 browser.storage.local.get('favorites', function (result) {
   favorites = result.favorites || [{ url: 'https://gmail.com', favicon: 'https://mailmeteor.com/logos/assets/PNG/Gmail_Logo_512px.png', id: 0 }, { url: 'https://music.youtube.com', favicon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Youtube_Music_icon.svg/2048px-Youtube_Music_icon.svg.png', id: 1 }];
+  console.log(favorites)
   loadFavorites()
 });
 
@@ -358,13 +359,12 @@ function loadFavorites() {
       })
       element.ariaLabel = "favopen";
       updateSearchBar();
-      await new Promise(resolve => setTimeout(resolve, 2500));
+      await new Promise(resolve => setTimeout(resolve, 2700));
       browser.tabs.query({ currentWindow: true }).then((tabs) => {
         favIcon.src = tabs.find((elems) => elems.id == element.id).favIconUrl
         browser.storage.local.get('favorites', function (result) {
           var favoritesc = result.favorites || [{ url: 'https://gmail.com', favicon: 'https://mailmeteor.com/logos/assets/PNG/Gmail_Logo_512px.png' }, { url: 'https://music.youtube.com', favicon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Youtube_Music_icon.svg/2048px-Youtube_Music_icon.svg.png' }];
           favoritesc[favorite.id].favicon = favIcon.src;
-          console.log(favoritesc)
           browser.storage.local.set({ favorites: favoritesc })
         });
       });
